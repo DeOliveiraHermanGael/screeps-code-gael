3af747aa468dcfb8cfd2591f933975960124a7ce
DeOliveiraHermanGael <165495907+DeOliveiraHermanGael@users.noreply.github.com>
2025-01-30 19:51:52 +0100

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -1,5 +1,7 @@
-logistics = require('Room.logistics').logistics
+logistics = require('Room.logistics')
+civilian = logistics.civilian
 closestTarget = require('creep.path').closestTarget
+artificialSources = logistics.artificialSources
 module.exports = {
     run: function(creep) {
         if (creep.store.getFreeCapacity() === 0 && creep.memory.harvesting == true) {
@@ -8,32 +10,30 @@ module.exports = {
             creep.say('full')
         } else if (!creep.memory.harvesting && creep.store.getUsedCapacity() === 0) {
             creep.memory.harvesting = true
-            creep.room.memory.updateResupply = true
             creep.memory.targetSwitch = true
             creep.say('empty')
         } 
-        if (creep.memory.booleans.harvesting) {
+        if (creep.memory.harvesting) {
             creep.say('harvesting')
-            const source = closestTarget(creep, logistics[0].Esources.get(creep.room))
+            const source = closestTarget(creep, artificialSources.get(creep.room))
             if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(source)
             }
         } else {
-            if (logistics[2].primaryToSupply.get(creep.room).length > 0) {
-                const target = closestTarget(creep, logistics2.primaryToSupply(creep.room))
+            if (civilian[2].primaryToSupply.get(creep.room).length > 0) {
+                const target = closestTarget(creep, civilian.primaryToSupply.get(creep.room))
                 creep.say('supplying')
                 if (target.store[RESOURCE_ENERGY] == target.store.getCapacity(RESOURCE_ENERGY)) {
                     creep.memory.targetSwitch = true
-                    creep.room.memory.updatePrimaryToSupply = true
                 } else if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(target)
                 } else if (creep.transfer(target, RESOURCE_ENERGY) == OK) {
                     creep.memory.targetSwitch = true
                     if (target.store.getUsedCapacity > 500) {
-                        creep.room.memory.updatePrimary_heavyToResupply = true
+                        civilian[2].primary_heavyToSupply.initialize(creep.room)
                     }
                     if (target.store[RESOURCE_ENERGY] == target.store.getCapacity[RESOURCE_ENERGY]) {
-                        creep.room.memory.updatePrimaryToSupply = true
+                        civilian[2].primaryToSupply.initialize(creep.room)
                     }
                 } 
             } else if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
@@ -42,7 +42,6 @@ module.exports = {
         }
     }
 }
-
 
 
 
